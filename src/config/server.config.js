edb8406cf4f73ea184a29aa27267d06416b7873c
Nivashawk <46569236+Nivashawk@users.noreply.github.com>
2022-01-28 12:44:23 +0530

    Update server.config.js

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   host: "0.0.0.0",
-  port: 5000,
+  port: 8000,
   dbUrl:
-    "mongodb+srv://vijayvijay1997:sunder.vj@cluster0.s3gkt.mongodb.net/BUS-TRACKING_TEST",
+    "mongodb+srv://bustracking:123@cluster0.ufout.mongodb.net/BUS-TRACKING_TEST",
   adminCollection: "Admin",
   studentCollection: "Student",
   driverCollection: "Driver",
