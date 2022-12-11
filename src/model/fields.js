68d3c308142d04460e5c3df2c5440f871bfbd75f
uchihasharukh <39857984+uchihasharukh@users.noreply.github.com>
2022-12-11 17:48:42 +0530

    tracker id fix

--- src/model/fields.js
+++ src/model/fields.js
@@ -1,27 +1,27 @@
 const busFields = {
-    busNumber: 1,
-    busIdNumber: 1,
-    busRoute: 1,
-    busDriverName: 1,
-    busDriverId: 1,
-    isAssigned: 1
-  };
+  busNumber: 1,
+  busIdNumber: 1,
+  busRoute: 1,
+  busDriverName: 1,
+  busDriverId: 1,
+  trackerId: 1,
+  isAssigned: 1,
+};
 
-  const busFieldsNeedIds = {
-    busNumber: 1,
-    busIdNumber: 1
-  };
-  
-  const driverFields = {
-    name: 1,
-    phoneNumber: 1,
-    idNumber: 1,
-    isStarted: 1
-  };
-  
-  module.exports = {
-    busFields,
-    driverFields,
-    busFieldsNeedIds
-  };
-  
+const busFieldsNeedIds = {
+  busNumber: 1,
+  busIdNumber: 1,
+};
+
+const driverFields = {
+  name: 1,
+  phoneNumber: 1,
+  idNumber: 1,
+  isStarted: 1,
+};
+
+module.exports = {
+  busFields,
+  driverFields,
+  busFieldsNeedIds,
+};
